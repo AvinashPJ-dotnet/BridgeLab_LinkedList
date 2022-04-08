12ab37cab8f982eeac444997a921d79212ea38b3
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-08 14:18:58 +0530

    Added: INode interface and MyLinkedList class

--- src/com/bl/linked_list/storage/Main.java
+++ src/com/bl/linked_list/storage/Main.java
@@ -2,10 +2,5 @@ package com.bl.linked_list.storage;
 
 public class Main {
     public static void main(String[] args) {
-        MyNode<Object> myFirstNode = new MyNode<>(56);
-        MyNode<Object> mySecondNode = new MyNode<>(30);
-        MyNode<Object> myThirdNode = new MyNode<>(70);
-        myFirstNode.setNext(mySecondNode);
-        mySecondNode.setNext(myThirdNode);
     }
 }
---
+++ src/com/bl/linked_list/storage/test_case/MyLinkedListTest.java
@@ -0,0 +1,23 @@
+package com.bl.linked_list.storage.test_case;
+
+import com.bl.linked_list.storage.MyLinkedList;
+import com.bl.linked_list.storage.MyNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyLinkedListTest {
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldAddedToTop() {
+        MyNode<Object> myFirstNode = new MyNode<>(70);
+        MyNode<Object> mySecondNode = new MyNode<>(30);
+        MyNode<Object> myThirdNode = new MyNode<>(56);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.printMyNode();
+        boolean result = myLinkedList.head.equals(myThirdNode) &&
+                myLinkedList.tail.equals(myFirstNode);
+        Assertions.assertTrue(result);
+    }
+}
